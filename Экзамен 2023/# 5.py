a34739f6e4581545aa764a145243c29c86369458
CreeperBoom07 <nmartianoff07@yandex.ru>
2023-07-16 23:33:45 +0300

    Добавлены решения некоторых задач 13 с сайта КЕГЭ

--- Экзамен 2023/# 5.py
+++ Экзамен 2023/# 5.py
@@ -5,22 +5,15 @@ def tr(x: int) -> str:
         x //= 3
     return s
 
-def f(n):
+lst = []
+for n in range(1, 1000):
     r = tr(n)
     if n % 3 == 0:
         r += r[-2:]
     else:
-        r += tr(n % 3 * 5)
-    # if int(r, 3) > 133:
-    print(int(r, 3))
-f(11)
-f(12)
-# for n in range(1, 1000):
-#     r = tr(n)
-#     if n % 3 == 0:
-#         r += r[-2:]
-#     else:
-#         r += tr(n % 3 * 5)
-#     if int(r, 3) > 133:
-#         print(int(r, 3))
-#         break
+        r += tr((n % 3) * 5)
+    if int(r, 3) > 133:
+        lst.append(int(r, 3))
+        # print(n, int(r, 3))
+print(min(lst))
+# 141
