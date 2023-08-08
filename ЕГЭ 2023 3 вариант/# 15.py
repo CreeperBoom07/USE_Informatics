17223873c64122df0b92f67303deba14355b6c58
CreeperBoom07 <nmartianoff07@yandex.ru>
2023-08-08 21:31:10 +0300

    1. Добавлено частичное решение варианта OpenFIPI 2. Добавлено 9-е задание

--- ЕГЭ 2023 3 вариант/# 15.py
+++
@@ -1,7 +0,0 @@
-def f(x, y):
-    return (x+2*y > a) or (y < x) or (x < 30)
-
-
-for a in range(0, 1000):
-    if all(f(x, y) for y in range(0, 150) for x in range(0, 150)):
-        print(a)
--- ЕГЭ 2023 3 вариант/# 17.py
+++
@@ -1,9 +0,0 @@
-with open('17_9840.txt') as file:
-    total = []
-    lst = [int(i) for i in file]
-    max_lst = max([i for i in lst if len(str(abs(i))) == 4 and str(i)[-2:] == '39'])**2
-    for i in range(len(lst)-1):
-        if ((len(str(abs(lst[i]))) == 4) + (len(str(abs(lst[i+1]))) == 4)) == 1:
-            if (lst[i] + lst[i+1])**2 <= max_lst:
-                total.append(lst[i] + lst[i+1])
-print(len(total), max(total), sep='\n')
