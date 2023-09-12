93f90389000555d8d87267237e3eaef3d302062f
CreeperBoom07 <nmartianoff07@yandex.ru>
2023-09-12 23:41:26 +0300

    Добавлены решения задач 6 из д/з

--- Задание 6 Исполнитель черепаха/Дз/# 18.py
+++ Задание 6 Исполнитель черепаха/Дз/# 18.py
@@ -1,6 +1,6 @@
 k = 0
 for x in range(-200, 200):
     for y in range(-200, 200):
-        if y > x and y < 90 and y < 3*x:
+        if y < 3*x and y < 15 and y > (x-56)/4 and y > 0:
             k += 1
 print(k)
